2171e3c8f9e7360e71b07dc19d7904ac18b1f6d9
Krishna Mohan Koyya <krishna@glarimy.com>
2017-02-22 11:30:13 +0530

    Java Code

---
+++ glarimy-bank-server-2.0.0/src/main/java/com/glarimy/bank/Transaction.java
@@ -0,0 +1,102 @@
+
+package com.glarimy.bank;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{http://www.glarimy.com/bank/}type"/>
+ *         &lt;element ref="{http://www.glarimy.com/bank/}amount"/>
+ *         &lt;element ref="{http://www.glarimy.com/bank/}balance"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "type",
+    "amount",
+    "balance"
+})
+@XmlRootElement(name = "transaction")
+public class Transaction {
+
+    @XmlElement(required = true)
+    protected String type;
+    protected double amount;
+    protected double balance;
+
+    /**
+     * Gets the value of the type property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the value of the type property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setType(String value) {
+        this.type = value;
+    }
+
+    /**
+     * Gets the value of the amount property.
+     * 
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * Sets the value of the amount property.
+     * 
+     */
+    public void setAmount(double value) {
+        this.amount = value;
+    }
+
+    /**
+     * Gets the value of the balance property.
+     * 
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * Sets the value of the balance property.
+     * 
+     */
+    public void setBalance(double value) {
+        this.balance = value;
+    }
+
+}
